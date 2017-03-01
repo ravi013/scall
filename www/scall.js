f8299c2804ac2dd2b0b1e6f3c3bb5d575b36c69a
ravi d suvarna <ravi013@gmail.com>
2017-03-01 17:52:44 +0530

    a

--- www/scall.js
+++ www/scall.js
@@ -48,7 +48,7 @@ function Scall() {
     var me = this;
 
     channel.onCordovaReady.subscribe(function() {
-        me.getInfo(function(info) {
+        me.call(function(info) {
             //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
             //TODO: CB-5105 native implementations should not return info.cordova
             var buildLabel = cordova.version;
