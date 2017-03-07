136a9b4b973c7dae8691c03df56f79f29e7517c0
ravi <ravi013@gmail.com>
2017-03-07 13:54:53 +0530

    n

--- src/android/Scall.java
+++ src/android/Scall.java
@@ -1,21 +1,21 @@
 /*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
-*/
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
 package org.apache.cordova.scall;
 
 import java.util.TimeZone;
@@ -31,23 +31,26 @@ import org.json.JSONObject;
 import android.provider.Settings;
 
 import com.sightcall.universal.Universal;
+import com.sightcall.universal.event.UniversalStatusEvent;
+
+import net.rtccloud.sdk.Event;
 
 public class Scall extends CordovaPlugin {
     public static final String TAG = "Scall";
-
+    
     public static String platform;                            // Scall OS
     public static String uuid;                                // Scall UUID
-
+    
     private static final String ANDROID_PLATFORM = "Android";
     private static final String AMAZON_PLATFORM = "amazon-fireos";
     private static final String AMAZON_DEVICE = "Amazon";
-
+    
     /**
      * Constructor.
      */
     public Scall() {
     }
-
+    
     /**
      * Sets the context of the Command. This can then be used to do things like
      * get file paths associated with the Activity.
@@ -59,7 +62,17 @@ public class Scall extends CordovaPlugin {
         super.initialize(cordova, webView);
         Scall.uuid = getUuid();
     }
-
+    @Event
+    public final void onUniversalStatusEvent(UniversalStatusEvent event) {
+        switch (event.status()) {
+            case INITIALIZING:
+                // Universal SDK is loading: Notify the user
+                break;
+            case IDLE:
+                // Universal SDK becomes idle: Remove any pending notification
+                break;
+        }
+    }
     /**
      * Executes the request and returns PluginResult.
      *
@@ -80,7 +93,7 @@ public class Scall extends CordovaPlugin {
         else  if ("call".equals(action)) {
             final JSONObject options = args.getJSONObject(0);
             final String url = options.getString("url");
-
+            
             Universal.start(url  /*java.lang.String*/);
             callbackContext.success();
         }
@@ -89,11 +102,11 @@ public class Scall extends CordovaPlugin {
         }
         return true;
     }
-
+    
     //--------------------------------------------------------------------------
     // LOCAL METHODS
     //--------------------------------------------------------------------------
-
+    
     /**
      * Get the OS name.
      *
@@ -108,7 +121,7 @@ public class Scall extends CordovaPlugin {
         }
         return platform;
     }
-
+    
     /**
      * Get the device's Universally Unique Identifier (UUID).
      *
@@ -118,27 +131,27 @@ public class Scall extends CordovaPlugin {
         String uuid = Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
         return uuid;
     }
-
+    
     public String getModel() {
         String model = android.os.Build.MODEL;
         return model;
     }
-
+    
     public String getProductName() {
         String productname = android.os.Build.PRODUCT;
         return productname;
     }
-
+    
     public String getManufacturer() {
         String manufacturer = android.os.Build.MANUFACTURER;
         return manufacturer;
     }
-
+    
     public String getSerialNumber() {
         String serial = android.os.Build.SERIAL;
         return serial;
     }
-
+    
     /**
      * Get the OS version.
      *
@@ -148,18 +161,18 @@ public class Scall extends CordovaPlugin {
         String osversion = android.os.Build.VERSION.RELEASE;
         return osversion;
     }
-
+    
     public String getSDKVersion() {
         @SuppressWarnings("deprecation")
         String sdkversion = android.os.Build.VERSION.SDK;
         return sdkversion;
     }
-
+    
     public String getTimeZoneID() {
         TimeZone tz = TimeZone.getDefault();
         return (tz.getID());
     }
-
+    
     /**
      * Function to check if the device is manufactured by Amazon
      *
@@ -171,10 +184,10 @@ public class Scall extends CordovaPlugin {
         }
         return false;
     }
-
+    
     public boolean isVirtual() {
-	return android.os.Build.FINGERPRINT.contains("generic") ||
-	    android.os.Build.PRODUCT.contains("sdk");
+        return android.os.Build.FINGERPRINT.contains("generic") ||
+        android.os.Build.PRODUCT.contains("sdk");
     }
-
+    
 }
